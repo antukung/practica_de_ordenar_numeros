185b011f88446b99bc85495013b352dd04fabb7d
Antu <122485403+antukung@users.noreply.github.com>
2023-06-28 19:17:27 -0300

    aca esta correguido
    seguimos con esto gente este es mi porfolio

--- bingoganador.py
+++ bingoganador.py
@@ -5,14 +5,20 @@ JAJAJAJA QUE LINDO PROGRAMAR"""
 
 numeros_ganadores=[6,21,45,7]
 
-dar_un_numero= input("dame un numero")
-dar_un_numero2=input("dame el segundo numero")
-dar_un_numero3=input("dame el tercer numero")
-dar_un_numero4=input("dame el cuarto numero")
+dar_un_numero= int(input("dame un numero"))
+dar_un_numero2=int(input("dame el segundo numero"))
+dar_un_numero3=int(input("dame el tercer numero"))
+dar_un_numero4=int(input("dame el cuarto numero"))
 
-for i in range(len(numeros_ganadores)):
-    elnumero=numeros_ganadores=[i]
-    if (elnumero==dar_un_numero) or (elnumero==dar_un_numero2) or (elnumero==dar_un_numero3) or (elnumero==dar_un_numero4):
-        print ("TIENES AL MENOS UN NUMERO GANDOR")
-    else:
-        print ("NINGUN NUMERO ES GANADOR")
+hay_ganador= False
+
+for i in numeros_ganadores:
+    
+    if i==dar_un_numero or i==dar_un_numero2 or i==dar_un_numero3 or i==dar_un_numero4:
+        hay_ganador= True
+        break
+
+if hay_ganador:
+    print ("HAY UN NUMERO GANADOR")
+else:
+    print ("SON TODOS PESIMOS NUMEROS")
